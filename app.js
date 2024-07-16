1df538a1e4430b6b43cbe6e252d88a30615f47a5
aditya-075 <146202236+aditya-075@users.noreply.github.com>
2024-07-16 20:50:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ const server = app.listen(PORT, ()=>{
 const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine","ejs")
 
-let socketsConnected = new Set()
+
+let socketsConnected = new Set();
+//let participants = new Set();
+let map = new Map();
 
 io.on('connection',onConnected)
 
@@ -20,19 +24,34 @@ function onConnected(socket){
     socketsConnected.add(socket.id);
 
     io.emit('clients-total',socketsConnected.size);
+    
+    socket.on('user-joined', (data) => {
+        map.set(socket.id, data.name);
+        io.emit('update-participants', Array.from(map));
+    });
 
     socket.on('disconnect',()=>{
         console.log('socket Disconnected',socket.id);
         socketsConnected.delete(socket.id);
         io.emit('clients-total',socketsConnected.size);
+        map.delete(socket.id);
+        io.emit('update-participants',Array.from(map));
     })
 
     socket.on('message',(data)=>{
-        console.log(data);
+        //console.log(data);
         socket.broadcast.emit('chat-message',data)
+        //console.log(data.name);
+        // if(!participants.has(data.name)){
+        //     participants.add(data.name);
+        //     io.emit('update-participants',Array.from(participants));
+        // }
+        map.set(socket.id,data.name);
+        io.emit('update-participants',Array.from(map));
     })
-
+    
     socket.on('feedback',data=>{
         socket.broadcast.emit('feedback',data)
-    })
+    }) 
+    
 }
